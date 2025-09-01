6f50901a786a6fd52cd12ad8f295b3f097a54877
luizkikinho <luiz.caetano2@fatec.sp.gov.br>
2025-09-01 20:01:43 -0300

    Organizado o MVC

--- ex03/controllers/ProdutoController.php
+++ ex03/controllers/ProdutoController.php
@@ -1,6 +1,10 @@
 <?php
+ini_set('display_erros', 1);
+error_reporting(E_ALL);
+
 require_once 'model.php';
-if ($_SERVER['REQUEST_METHOD']=="POST") {
+
+if (isset($_POST['Gravar'])) {
   $codigo=$_POST['cx_codigo'];
   $produto=$_POST['cx_produto'];
   $validade=$_POST['cx_validade'];
